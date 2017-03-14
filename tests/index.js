a96b44d2039781e2aaa590eac9bf83056c378c2f
Ajith Ranka <ajith@mapbox.com>
2017-03-14 15:37:58 +0530

    use consts, change msg

--- tests/index.js
+++ tests/index.js
@@ -5,11 +5,11 @@ const parser = require('../index.js');
 const filenames = fs.readdirSync('tests/data', { encoding: 'utf-8' });
 
 filenames.forEach(filename => {
-  var tests = JSON.parse(fs.readFileSync(`tests/data/${filename}`));
+  const tests = JSON.parse(fs.readFileSync(`tests/data/${filename}`));
   tests.forEach(({input, output : expectedOutput}, i) => {
     tape(`testing ${filename}, test ${i + 1}`, t => {
-      var actualOutput = parser(input);
-      t.deepEqual(actualOutput, expectedOutput, 'parsed correctly');
+      const actualOutput = parser(input);
+      t.deepEqual(actualOutput, expectedOutput, 'parsing');
       t.end();
     })
   });
